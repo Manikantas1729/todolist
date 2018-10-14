6a45de5548022c0c0a4ca8f8e1231e687817815d
Manikanta <manikantas1729@gmail.com>
2018-10-14 13:43:29 +0530

    Update todos/models.py

--- todos/models.py
+++ todos/models.py
@@ -4,13 +4,18 @@ import datetime
 from django.db import models
 from unittest.util import _MAX_LENGTH
 from django.views.decorators.http import last_modified
-fields = ('Inprogress','completed','pending')
 # Create your models here.
 class ToDo(models.Model):
     title = models.CharField(max_length=200)
     Description = models.TextField()
     status = models.Value(fields)
     created_at = models.DateTimeField(default=datetime.time,blank = True)
+    stat = (
+            ('a','In progress'),
+            ('b','completed'),
+            ('c','pending'),
+           )
+    status = models.CharField(max_length=100,default = 'pending',choices=stat)
     last_modified
     def __str__(self):
         return self.title
